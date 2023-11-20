aa43826159a0ea80e25097d481abbd08a2ee5ec1
torimize <vmize@pdx.edu>
2023-11-20 12:15:42 -0800

    Project 2 Update
    Updated Work

--- project-2/script.js
+++ project-2/script.js
@@ -31,3 +31,20 @@ for (var i = 0; i < navItems.length; i++) {
       this.classList.add("active");
    });
 }
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+          
