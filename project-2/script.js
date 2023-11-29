23b7dccf54dc06224db6e31543cf97300affb7a6
torimize <vmize@pdx.edu>
2023-11-29 12:23:33 -0800

    Update
    Update

--- project-2/script.js
+++ project-2/script.js
@@ -27,15 +27,105 @@ for (var i = 0; i < navItems.length; i++) {
    });
 }
 
-$('.my-slick').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay:true,
-  autoplaySpeed:3500,
-  infinite:true,
-  dots:true,
-  draggable:false,
+const slideList = [
+  {
+  id: 1,
+  text: 'first slide',
+  src: 'images/inside-a.png'
+},
+{
+  id: 2,
+  text: 'second slide',
+  src: 'images/inside-b.png'
+},
+{
+  id: 3,
+  text: 'third slide',
+  src: 'images/inside-c.png'
+},
+  {
+  id: 4,
+  text: 'fourth slide',
+  src: 'images/inside-d.png'
+},
+]
+
+let count = 0;
+let numSlides = 4;
+
+
+function increaseCount() { 
+$('.next').click(function () { 
+  count++
+  console.log(count)
+  if (count < numSlides) {
+    renderSlide(count)
+  } else { 
+    count = 0
+    renderSlide(count)
+  }
 })
+return count
+
+}
+function decreaseCount() { 
+$('.prev').click(function () { 
+
+  if (count > 0) {
+    count--
+    renderSlide(count)
+    console.log(count)
+  }
+  else {
+    count = numSlides - 1
+    renderSlide(count)
+  }
+})
+
+return count
+
+}
+
+function renderSlide(count) { 
+$('#slideContent').html(`
+    <!-- id: ${slideList[count].id} -->
+    <h2>${slideList[count].text}</h2>
+    <img class="slideimg" src=${slideList[count].src}>
+`)
+}
+
+
+
+
+$(decreaseCount())
+$(increaseCount())
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("add");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
   
   //when the page loads call toggleIcon;
